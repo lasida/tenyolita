2d1f634159f55829200955267b9275acbe48e902
Chariselyasa123 <ahmad.charis@raharja.info>
2022-08-23 13:44:11 +0700

    add dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">id</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">No WhatsApp</th>
+                                <th scope="col">Domisili</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Created At</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($data as $value)
+                                <tr>
+                                    <th scope="row">{{ $loop->iteration }}</th>
+                                    <td>{{ $value->name }}</td>
+                                    <td>{{ $value->email }}</td>
+                                    <td>{{ $value->whatsapp }}</td>
+                                    <td>{{ $value->city }}</td>
+                                    <td>{{ $value->is_sent }}</td>
+                                    <td>{{ $value->created_at }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
